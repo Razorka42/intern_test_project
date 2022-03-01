ba50f06f7143a70773ad97d6a9f3115ff25c825d
Razorka42 <razorka.com@gmail.com>
2022-03-01 20:48:14 +0700

    Implements logging for exceptions handlers.

--- src/main/java/com/setronica/intern/test/project/exception/ExceptionsHandler.java
+++ src/main/java/com/setronica/intern/test/project/exception/ExceptionsHandler.java
@@ -1,5 +1,6 @@
 package com.setronica.intern.test.project.exception;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.TypeMismatchException;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpHeaders;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.validation.ConstraintViolation;
@@ -20,23 +22,28 @@ import java.util.Map;
 
 @ControllerAdvice
 public class ExceptionsHandler extends ResponseEntityExceptionHandler {
+    private static final Logger log = Logger.getLogger("Exception logger");
+
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<?> onResourceNotFoundException() {
+    public ResponseEntity<?> onResourceNotFoundException(ResourceNotFoundException ex) {
+        log.info("Resource not found " + ex.getClass().getSimpleName() + " " + ex.getMessage());
         return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(ResourceQueryNotFoundException.class)
     public ResponseEntity<NotFoundError> onResourceQueryNotFoundException(ResourceQueryNotFoundException ex) {
+        log.info("Resource query is not correct " + ex.getClass().getSimpleName() + " " + ex.getMessage());
         return new ResponseEntity<>(new NotFoundError(HttpStatus.NOT_FOUND.value(), ex.getMessage()), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<Map<String, String>> onConstraintViolationException(ConstraintViolationException e) {
+    public ResponseEntity<Map<String, String>> onConstraintViolationException(ConstraintViolationException ex) {
         Map<String, String> errors = new HashMap<>();
-        for (ConstraintViolation violation : e.getConstraintViolations()) {
+        for (ConstraintViolation violation : ex.getConstraintViolations()) {
             errors.put(violation.getPropertyPath().toString(), violation.getMessage());
         }
+        log.warn("ConstraintViolationException " + ex.getClass().getSimpleName() + " " + ex.getMessage());
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
@@ -47,17 +54,26 @@ public class ExceptionsHandler extends ResponseEntityExceptionHandler {
         for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
             errors.put(fieldError.getField(), fieldError.getDefaultMessage());
         }
+        log.warn("Method argument not valid " + ex.getClass().getSimpleName() + " " + ex.getMessage());
         return new ResponseEntity<>(errors, status);
     }
 
     @Override
     protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        log.warn("Http Message Not Readable " + ex.getClass().getSimpleName() + " " + ex.getMessage());
         return new ResponseEntity<>(ex.getMessage(), status);
     }
 
     @Override
     protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        log.warn("Type Mismatch " + ex.getClass().getSimpleName() + " " + ex.getMessage());
         return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        log.error("Unhandled exception " + ex.getClass().getSimpleName() + " " + ex.getMessage());
+        return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 
--- src/main/resources/db/migration/V1_1__create_tables_Price_and_Translations.sql
+++ src/main/resources/db/migration/V1_1__create_tables_Price_and_Translations.sql
@@ -2,14 +2,14 @@ DROP TABLE IF EXISTS price;
 
 CREATE TABLE price
 (
-    id          bigserial primary key,
+    id         bigserial primary key,
     product_id bigserial,
-    value       decimal(64, 2) NOT NULL,
-    currency        varchar(3)   NOT NULL,
-    FOREIGN KEY (product_id)  REFERENCES product (id)
+    value      decimal(64, 2) NOT NULL,
+    currency   varchar(3)     NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES product (id)
 
 );
 ALTER TABLE product
-DROP column price;
+    DROP column price;
 
 
