2c9cde6c65bd36e69504176be3301d5fbc02f25e
Razorka42 <razorka.com@gmail.com>
2022-03-01 21:28:17 +0700

    Add Swagger documentation auto generated.

--- src/main/resources/db/migration/V1_2__create_table_Translations.sql
+++ src/main/resources/db/migration/V1_2__create_table_Translations.sql
@@ -3,16 +3,16 @@ DROP TABLE IF EXISTS translations;
 CREATE TABLE translations
 (
     id          bigserial primary key,
-    product_id  bigserial,
-    name        varchar(255) NOT NULL,
+    product_id bigserial,
+    name varchar(255) NOT NULL,
     description varchar(255),
-    language    varchar(3)   NOT NULL,
-    FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE CASCADE
+    language varchar(3) NOT NULL,
+    FOREIGN KEY (product_id)  REFERENCES product (id) ON DELETE CASCADE
 
 );
 ALTER TABLE product
-    DROP column name;
+DROP column name;
 ALTER TABLE product
-    DROP column description;
+DROP column description;
 
 
